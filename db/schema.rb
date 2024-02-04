97ed0d6306cb6418c38bd9a76ae57c369cf95d7d
lily4178993 <nellytelli@gmail.com>
2024-02-04 04:41:27 +0000

    feat(schema): Add reservation table to the database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_04_043118) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_04_043925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,10 +27,24 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_04_043118) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.time "start_time"
+    t.time "end_time"
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "mentor_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mentor_id"], name: "index_reservations_on_mentor_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "mentors"
+  add_foreign_key "reservations", "users"
 end

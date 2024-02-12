b50672b221341c8ff4f02e514083db4e9dccf302
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-12 16:46:52 +0300

    Add remove and restore api route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      get 'removed_mentors', to: 'mentors#removed_mentors'
       resources :mentors, only: [:create, :index, :show, :destroy] do
         patch 'remove_mentor', on: :member
         patch 'restore_mentor', on: :member

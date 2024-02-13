0c486f011f7b028072a34409fe785863aa04386a
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-13 14:03:27 +0100

    Pull routes from develop branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  # Define API routes
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
-      resources :mentors, only: [:create, :index, :show, :destroy]
+      get 'removed_mentors', to: 'mentors#removed_mentors'
+      resources :mentors, only: [:create, :index, :show, :destroy] do
+        patch 'remove_mentor', on: :member
+      end
+      resources :users, only: [:create,:index, :show] do
+        get 'users', to: 'users#index', on: :collection
+        get 'reservations', to: 'users#user_reservations', on: :member
+        get 'find_by_username', to: 'users#find_by_username', on: :collection
+      end
       resources :reservations, only: [:create, :index]
     end
   end

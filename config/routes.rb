1c66d4e375deecdb0da67ab2d3bdc2c37b236cc6
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-29 18:39:32 +0100

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
+  root to: redirect('/api-docs')
+  
   namespace :api do
     namespace :v1 do
       get 'removed_mentors', to: 'mentors#removed_mentors'
